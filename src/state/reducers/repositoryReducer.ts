6b12dd4660275b8d643674ead4cbbe02865dc736
mbukeRepo <mbuke404@gmail.com>
2022-08-16 12:09:57 +0200

    created interface foreach action and created enum for actionTypes

--- src/state/reducers/repositoryReducer.ts
+++ src/state/reducers/repositoryReducer.ts
@@ -4,26 +4,48 @@ interface RepositoriesState {
     loading: boolean
 }
 
-interface Action {
-    type: string;
-    payload?: any;
+interface SearchRepositoriesAction {
+    type: "search_repositories"
 }
 
- const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
+interface SearchRepositoriesSuccessAction{
+    type: "search_repositories_success";
+    payload: string[];
+}
+
+interface SearchRepositoriesErrorAction{
+    type: "search_repositories_error";
+    payload: string;
+}
+
+type Action =   SearchRepositoriesAction 
+| SearchRepositoriesErrorAction 
+| SearchRepositoriesSuccessAction;
+
+enum actionType  {
+    SEARCH_REPOSITORIES = "search_repositories",
+    SEARCH_REPOSITORIES_SUCCESS = "search_repositories_success",
+    SEARCH_REPOSITORIES_ERROR = "search_repositories_error"
+}
+
+const reducer = (
+    state: RepositoriesState, 
+    action:Action
+    ): RepositoriesState => {
     switch(action.type){
-        case "search_repositories":
+        case actionType.SEARCH_REPOSITORIES:
             return {
                 loading: true,
                 error: null,
                 data: []
             }
-        case "search_repositories_success":
+        case actionType.SEARCH_REPOSITORIES_SUCCESS:
             return {
                 data: action.payload,
                 error: null,
                 loading: false
             }
-        case "search_repositories_error":
+        case actionType.SEARCH_REPOSITORIES_ERROR:
             return {
                 data: [],
                 error: action.payload,
