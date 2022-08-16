0ab62ebadf45c0389f7cfa880829a64808d13dee
mbukeRepo <mbuke404@gmail.com>
2022-08-16 15:25:18 +0200

    consumed redux state

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,14 +1,40 @@
-import React from "react";
-import { Provider } from "react-redux";
-import {store} from "../state/store";
-const providerProps = {
-    store
-}
+import React, {useState} from "react";
+
+
+import SearchBox from "../SearchBox/SearchBox";
+import {useActions} from "../hooks/useActions";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+
+
 const App = () => {
+    const [term, setTerm] = useState("");
+    const {searchRepositories} = useActions();
+    const {data} = useTypedSelector(state => state.repositories);
+    
+
+    const onChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+        setTerm(e.target.value);
+    }
+
+    const onSubmit:React.FormEventHandler<HTMLFormElement> = (e) => {
+        e.preventDefault();
+        searchRepositories(term);
+        setTerm("");
+    }
+
+    const searchBoxProps = {
+        onChange,
+        onSubmit,
+        term
+    }
+
     return (
-        <Provider {...providerProps}>
-            <h1>Hello world</h1>
-        </Provider>
+        <div>
+            <h1>Search npm packages</h1>
+            <SearchBox {...searchBoxProps} />
+            {data.length !== 0  && data.map(term => (<p key={term}>{term}</p>))}
+        </div>
+        
     );
 }
 
